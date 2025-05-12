b01ad5a146fc45dcafc2610fc4be86756260bcb0
fadi <fadishqerat54@icloud.com>
2025-05-12 23:12:14 +0300

    still try2
    still try2

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   images: {
     domains: ['cdn.sanity.io']
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@prisma/client');
-    }
-    return config;
+    
   }
+  
+
 };
+
 
 export default nextConfig;
