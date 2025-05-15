81414d09ec70fc4a411b106744dee2a709c450b0
fadi <fadishqerat54@icloud.com>
2025-05-15 17:51:54 +0300

    f
    f

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -3,7 +3,8 @@ import prisma from "./db";
 import { encodeBase32LowerCaseNoPadding, encodeHexLowerCase } from "@oslojs/encoding";
 import { sha256 } from "@oslojs/crypto/sha2";
 import { cookies } from "next/headers";
-import type { User,Session } from "@prisma/client";
+import type { User,Session } from "@/generated/prisma/client";
+
 import { cache } from "react";
 
 
