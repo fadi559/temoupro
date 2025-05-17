331391859d6b3cd5a04b521a2edca3e27d3a7879
fadi <fadishqerat54@icloud.com>
2025-05-17 22:45:12 +0300

    Revert "x"
    This reverts commit 846901481d9dd9a459f63e093d1f6f51f0ae4f2c.

--- src/actions/db.ts
+++ src/actions/db.ts
@@ -1,5 +1,18 @@
-import { PrismaClient } from "@prisma/client";
+// db.ts
+import pkg from '@prisma/client';
+const { PrismaClient } = pkg;
 
-const prisma = new PrismaClient();
+let prisma: pkg.PrismaClient;
+
+if (typeof window === 'undefined') {
+  if (process.env.NODE_ENV === 'production') {
+    prisma = new PrismaClient();
+  } else {
+    if (!(global as any).prisma) {
+      (global as any).prisma = new PrismaClient();
+    }
+    prisma = (global as any).prisma;
+  }
+}
 
 export default prisma;
