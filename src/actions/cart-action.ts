df1bb45957ddb748132eaa9bf1285f12580c2473
fadi <fadishqerat54@icloud.com>
2025-05-17 14:17:46 +0300

    cousera edit
    cousera edit

--- src/actions/cart-action.ts
+++ src/actions/cart-action.ts
@@ -183,7 +183,7 @@ export const syncCartWithUser = async (cartId: string | null) => {
     }
 
     for(const item of existingAnonymousCart.items) {
-        const existingItem = existingUserCart.items.find((userItem) => userItem.sanityProductId === item.sanityProductId
+        const existingItem = existingUserCart.items.find((userItem: { sanityProductId: string }) => userItem.sanityProductId === item.sanityProductId
     );
 
         if(existingItem) {
