846901481d9dd9a459f63e093d1f6f51f0ae4f2c
fadi <fadishqerat54@icloud.com>
2025-05-17 15:29:05 +0300

    x
    my fix

--- src/actions/db.ts
+++ src/actions/db.ts
@@ -1,18 +1,5 @@
-// db.ts
-import pkg from '@prisma/client';
-const { PrismaClient } = pkg;
+import { PrismaClient } from "@prisma/client";
 
-let prisma: pkg.PrismaClient;
-
-if (typeof window === 'undefined') {
-  if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient();
-  } else {
-    if (!(global as any).prisma) {
-      (global as any).prisma = new PrismaClient();
-    }
-    prisma = (global as any).prisma;
-  }
-}
+const prisma = new PrismaClient();
 
 export default prisma;
